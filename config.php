c5f929024e0ab4ba5e4a10336b78f3f220fa74d2
Raiane Dev <89032013+Raiane-Dev@users.noreply.github.com>
2021-11-21 10:58:44 -0300

    Update

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    define('INCLUDE_PATH','http://localhost/Curso/Projeto/CreateWebsite/');
+    define('INCLUDE_PATH','http://localhost/CreateWebsite/');
     define('INCLUDE_PATH_ASSETS','http://localhost/Curso/Projeto/CreateWebsite/assets/');
 
 ?>
